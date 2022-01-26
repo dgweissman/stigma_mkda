31ace65f45e843eb70f8bfb7a346ca5e12e11315
David Weissman <47182392+dgweissman@users.noreply.github.com>
2022-01-26 14:01:22 -0800

    Update MKDA_and_logistic_regression.m

--- MKDA_and_logistic_regression.m
+++ MKDA_and_logistic_regression.m
@@ -16,7 +16,93 @@ Meta_Activation_FWE('all',DB,500,'nocontrasts','noverbose');
 load MC_Info
 [studybyroi,studybyset]=Meta_cluster_tools('getdata',r,MC_Setup.unweighted_study_data,MC_Setup.volInfo);
 img=fmri_data('Activation_FWE_all.img','noverbose');
-%Logistic regression
-voxel_for_loop
+%Load mkda results
+load MC_Info;
+
+%Run logistic regression of County Structural Stigma on each voxel from each study based on mkda CIMs
+tbrain=double.empty;
+for n = 1:231202
+    vals=MC_Setup.unweighted_study_data(n,:);
+    input=zeros(24,1);
+    input(find(vals==1))=1;
+    a= voxlogistic(CountySS,input,wts);
+    tbrain=cat(2,tbrain,a);   
+end
+tbrain=sparse(tbrain');
+save('CountySS_logistic_tscores.mat','tbrain');
+
 %Map back on to brain
 iimg_reconstruct_vols(tbrain,MC_Setup.volInfo,'outname','County_SS_metalog.img')
+
+%Run logistic regression of State Structural Stigma on each voxel from each study based on mkda CIMs
+tbrain=double.empty;
+for n = 1:231202
+    vals=MC_Setup.unweighted_study_data(n,:);
+    input=zeros(24,1);
+    input(find(vals==1))=1;
+    a= voxlogistic(StateSS,input,wts);
+    tbrain=cat(2,tbrain,a);    
+end
+tbrain=sparse(tbrain');
+save('StateSS_logistic_tscores.mat','tbrain');
+
+%Map back on to brain
+iimg_reconstruct_vols(tbrain,MC_Setup.volInfo,'outname','State_SS_metalog.img')
+
+%Run logistic regression of GINI on each voxel from each study based on mkda CIMs
+tbrain=double.empty;
+for n = 1:231202
+    vals=MC_Setup.unweighted_study_data(n,:);
+    input=zeros(24,1);
+    input(find(vals==1))=1;
+    a= voxlogistic(gini,input,wts);
+    tbrain=cat(2,tbrain,a);    
+end
+tbrain=sparse(tbrain');
+save('gini_logistic_tscores.mat','tbrain');
+%Map back on to brain
+iimg_reconstruct_vols(tbrain,MC_Setup.volInfo,'outname','gini_metalog.img')
+
+%Run logistic regression of pctblack on each voxel from each study based on mkda CIMs
+tbrain=double.empty;
+for n = 1:231202
+    vals=MC_Setup.unweighted_study_data(n,:);
+    input=zeros(24,1);
+    input(find(vals==1))=1;
+    a= voxlogistic(pctblack,input,wts);
+    tbrain=cat(2,tbrain,a);    
+end
+tbrain=sparse(tbrain');
+save('pctblack_logistic_tscores.mat','tbrain');
+
+%Map back on to brain
+iimg_reconstruct_vols(tbrain,MC_Setup.volInfo,'outname','pctblack_metalog.img')
+
+%Run logistic regression of pctcollege on each voxel from each study based on mkda CIMs
+tbrain=double.empty;
+for n = 1:231202
+    vals=MC_Setup.unweighted_study_data(n,:);
+    input=zeros(24,1);
+    input(find(vals==1))=1;
+    a= voxlogistic(pctcollege,input,wts);
+    tbrain=cat(2,tbrain,a);    
+end
+tbrain=sparse(tbrain');
+save('pctcollege_logistic_tscores.mat','tbrain');
+
+%Map back on to brain
+iimg_reconstruct_vols(tbrain,MC_Setup.volInfo,'outname','pctcollege_metalog.img')
+
+%Run logistic regression of IAT on each voxel from each study based on mkda CIMs
+tbrain=double.empty;
+for n = 1:231202
+    vals=MC_Setup.unweighted_study_data(n,:);
+    input=zeros(24,1);
+    input(find(vals==1))=1;
+    a= voxlogistic(IAT,input,wts);
+    tbrain=cat(2,tbrain,a);    
+end
+tbrain=sparse(tbrain');
+save('IAT_logistic_tscores.mat','tbrain');
+%Map back on to brain
+iimg_reconstruct_vols(tbrain,MC_Setup.volInfo,'outname','IAT_metalog.img')
